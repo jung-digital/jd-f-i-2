94a45f4544b154b3cf9cde4287cba29985ce55bb
joshjung <joshua.p.jung@gmail.com>
2016-01-24 20:48:49 -0600

    Fix watermarked image, move template into single javascript file, make menu items distribute horizontally automatically, and udpate the Products_Services.html to use the new menu

--- website/scripts/app.js
+++ website/scripts/app.js
@@ -1,6 +1,32 @@
-var jungleMenu = new Jungle.Menu({
-	container: document.querySelector(".jungle-menu"),
-	config: "scripts/menu-config.json",
-	isSticky: true
-});
+$(document).ready(function () {
+  var script = document.createElement('script');
 
+  script.setAttribute('type', 'text/template');
+  script.setAttribute('id', 'menuTemplate');
+
+  script.text = '<div class="menu-anchor"></div>\n' +
+    '<div class="menu">\n' +
+      '<ul class="row">\n' +
+        '<% items.forEach(function(item) { %>\n' +
+        '<li>\n' +
+          '<a href="<%= item.href %>" target="main">\n' +
+            '<div class="item <% if (item.isSelected) { print(\'active\') } %>">\n' +
+              '<%= item.name %>\n' +
+              '<% if (typeof item.subtitle !== "undefined") { %>\n' +
+              '<div class="subtitle"><%= item.subtitle %></div>\n' +
+              '<% }; %>\n' +
+            '</div>\n' +
+          '</a>\n' +
+        '</li>\n' +
+        '<% }); %>\n' +
+      '</ul>\n' +
+    '</div>\n';
+
+  document.body.appendChild(script);
+
+  var jungleMenu = new Jungle.Menu({
+    container: document.querySelector(".jungle-menu"),
+    config: "scripts/menu-config.json",
+    isSticky: true
+  });
+});
